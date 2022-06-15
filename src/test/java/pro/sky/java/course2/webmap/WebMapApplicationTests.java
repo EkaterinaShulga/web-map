7459c0df684478f86b35b697ed9face3aa18db3c
Екатерина <shulga23@gmail.com>
2022-06-15 14:25:33 +0300

    первый коммит

---
+++ src/test/java/pro/sky/java/course2/webmap/WebMapApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.webmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
